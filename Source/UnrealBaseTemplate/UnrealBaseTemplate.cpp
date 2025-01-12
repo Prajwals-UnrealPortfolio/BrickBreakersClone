b37cc1a2e320efd200219e260a8a9d2d3c1539fb
Prajwal <prajwalr.ramappa@gmail.com>
2025-01-12 16:27:50 +0530

    Initial Commit

---
+++ Source/UnrealBaseTemplate/UnrealBaseTemplate.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UnrealBaseTemplate.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UnrealBaseTemplate, "UnrealBaseTemplate" );
